f04c63284a3f9ac2d13bcef6bf4e48c9289155f9
Karol <karolile.ortiz@gmail.com>
2024-04-07 18:24:53 -0400

    EditProfile sin imports

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,12 +1,9 @@
 import { Component } from '@angular/core';
-import { MatIconModule } from '@angular/material/icon';
-import { FormsModule } from '@angular/forms';
-import { MatInputModule } from '@angular/material/input';
-import { MatFormFieldModule } from '@angular/material/form-field';
+
 @Component({
   selector: 'app-profile',
   standalone: true,
-  imports: [MatIconModule, FormsModule, MatInputModule, MatFormFieldModule ],
+  imports: [],
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.scss'
 })
