406647e35e4b569674c3fa427ed4960e8c791305
Karol <karolile.ortiz@gmail.com>
2024-04-17 23:04:19 -0400

    correcion validaciones editprofile

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
@@ -14,18 +14,12 @@ import { CommonModule } from '@angular/common';
 })
 export class ProfileComponent {
   profileForm: FormGroup = new FormGroup({
-    nombre: new FormControl('', Validators.required),
-    apellido: new FormControl('', Validators.required),
-    carnet: new FormControl('', Validators.required), 
-    correo: new FormControl('', [Validators.required, Validators.email]),
+    name: new FormControl('', Validators.required),
+    lastname: new FormControl('', Validators.required),
+    license: new FormControl('', Validators.required), 
+    mail: new FormControl('', [Validators.required, Validators.email]),
     password: new FormControl('', Validators.required)
   });
 
-  
-  onSubmit() {
-    if (this.profileForm.valid) {
-      
-      console.log(this.profileForm.value);
-    }
-  }
+
 }
