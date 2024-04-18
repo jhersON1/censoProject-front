60ef936fa3bb1fa289247ca6db813f23ecc89181
Karol <karolile.ortiz@gmail.com>
2024-04-17 20:27:07 -0400

    edit profile sin validaciones

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,13 +1,26 @@
-import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
+import { CommonModule } from '@angular/common';
+
 @Component({
   selector: 'app-profile',
   standalone: true,
-  imports: [FormsModule, MatInputModule],
+  imports: [MatFormFieldModule, MatInputModule, FormsModule, ReactiveFormsModule, MatButtonModule, CommonModule],
   templateUrl: './profile.component.html',
-  styleUrl: './profile.component.scss'
+  styleUrls: ['./profile.component.scss']
 })
 export class ProfileComponent {
+  profileForm: FormGroup = new FormGroup({
+    nombre: new FormControl(''),
+    apellido: new FormControl(''),
+    carnet: new FormControl(''), 
+    correo: new FormControl(''),
+    password: new FormControl('')
+  });
+
+  
 
 }
