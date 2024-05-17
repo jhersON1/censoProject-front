0017395ef50f7ed7763c6865f699cbdd319ebb6e
jherson <rodriguezpaul98@gmail.com>
2024-05-17 11:48:03 -0400

    delete unnecessary import

--- src/app/shared/navbar/nabbar.component.ts
+++ src/app/shared/navbar/nabbar.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component, inject, OnDestroy, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, OnInit } from '@angular/core';
 import { MatToolbar } from "@angular/material/toolbar";
 import { MatIcon } from "@angular/material/icon";
 import { MatButton, MatIconButton } from "@angular/material/button";
